1a7b8a9a2f4c1b4c974e680bc99635a70cdb506e
Esa Torkkila <esa.torkkila@gmail.com>
2022-12-18 13:37:58 +0200

    Fetch type effectiveness data

--- src/Components/Context.js
+++ src/Components/Context.js
@@ -4,7 +4,20 @@ const Context = createContext()
 function PokemonInfoContextProvider({children}) {
     //Show these in Info.js
     const [data, setData] = useState(['Poke1', 'Poke2', 'Poke3'])
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '467686f795msh5d709b323480269p1381a0jsn77efa3eaa824',
+            'X-RapidAPI-Host': 'pokemon-go1.p.rapidapi.com'
+        }
+    };
 
+    useEffect(() => {
+        fetch('https://pokemon-go1.p.rapidapi.com/type_effectiveness.json', options)
+            .then(response => response.json())
+            .then(response => setData(response))
+            .catch(err => console.error(err));
+    })
     return (
         <Context.Provider value={data}>
             {children}
