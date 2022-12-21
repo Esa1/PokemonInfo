85a2f09bbbe367fbe7b5c05634adc21845fe1d06
Esa Torkkila <esa.torkkila@gmail.com>
2022-12-21 10:45:15 +0200

    apiEndPoints.js file added. setFetchApi method added to Context so that data is needed only when needed.

---
+++ src/tools/apiEndPoints.js
@@ -0,0 +1,26 @@
+const apiEndPoints = [
+    type_effectiveness,
+    pokemon_rarity,
+    pokemon_powerup_requirements,
+    pokemon_genders,
+    current_pokemon_moves,
+    pokemon_evolutions,
+    raid_exclusive_pokemon,
+    pokemon_buddy_distances,
+    pokemon_candy_to_evolve,
+    pokemon_stats,
+    nesting_pokemon,
+    pokemon_max_cp,
+    shiny_pokemon,
+    alolan_pokemon,
+    fast_moves,
+    pokemon_names,
+    charged_moves,
+    released_pokemon,
+    weather_boosts,
+    possible_ditto_pokemon,
+    pokemon_types,
+    pokemon_encounter_data
+]
+
+export default apiEndPoints
